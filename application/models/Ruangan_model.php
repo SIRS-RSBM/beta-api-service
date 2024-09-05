51df0c965b999c97662baa2eb5d8c92fb0a6dea8
SIRS-RSBM <sirsrsbenmboi@gmail.com>
2024-09-06 07:41:02 +0800

    Update Ruangan_model.php

--- application/models/Ruangan_model.php
+++ application/models/Ruangan_model.php
@@ -1,17 +1,23 @@
-
 <?php
 class Ruangan_model extends CI_Model {
+    private $regonline;
+    private $master;
+
     public function __construct() {
         parent::__construct();
-        // Load database connection for 'regonline'
-        $this->load->database('default4', TRUE);
+        // Load database connections
+        $this->regonline = $this->load->database('default3', TRUE);  // Koneksi ke database 'regonline'
+        $this->master = $this->load->database('default', TRUE);     // Koneksi ke database 'master'
     }
 
     public function get_ruangan_list() {
-        // Tambahkan kondisi WHERE untuk memilih hanya baris dengan kolom 'antrian' = 'A'
-         $this->db->query("USE regonline");
-        $this->db->where('antrian', 'A');
-        $query = $this->db->get('link_ruangan');
+        // Melakukan join dengan database master, tabel penjamin_ruangan
+        $this->regonline->select('link_ruangan.*, penjamin_ruangan.ruangan_penjamin AS POLI_BPJS');
+        $this->regonline->from('link_ruangan');
+        $this->regonline->join('master.penjamin_ruangan', 'penjamin_ruangan.Ruangan_RS = link_ruangan.ID', 'left'); 
+        $this->regonline->where('link_ruangan.antrian', 'A');
+        $query = $this->regonline->get();
+
         return $query->result_array();
     }
 }
